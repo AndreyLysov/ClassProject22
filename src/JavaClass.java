10b92698dee2e1d6abcccdd5abbee2c35be4514e
andrey.lysov <lysandrey90@gmail.com>
2017-11-12 09:53:58 +0200

    changes in master branch

--- src/JavaClass.java
+++ src/JavaClass.java
@@ -3,5 +3,6 @@ public class JavaClass {
         System.out.println("hello");
         System.out.println("hi");
         System.out.println("123123");
+        System.out.println("Changes in master branch");
     }
 }
